3f0160f219a63e0777967fba943771754763e503
ManassesV <manavitorino@gmail.com>
2019-04-13 11:40:33 -0300

    asddas

--- projetoeducationlm/www/js/login.js
+++ projetoeducationlm/www/js/login.js
@@ -4,9 +4,41 @@ var $ = document;
  $.getElementById('login').addEventListener
  ('click', function(){
 
-    alert($.getElementById("first_name").value)
-    alert($.getElementById("last_name").value)
-    cordova.plugins.firebase.auth.createUserWithEmailAndPassword("manavitorino@gmail.com",
-     "pa55w0rd");
+    var email = $.getElementById("first_name")
+    var password = $.getElementById("last_name")
+    var lbemail = $.getElementById("lbemail")
+    var lbpassword = $.getElementById("lbpassword")
+    var contErro = 0;
+
+
+    if(email.value == ""){
+		lbemail.innerHTML = "Favor preencher o Email";
+		lbemail.style.display = 'block';
+		contErro += 1;
+	}else{
+		lbemail.style.display = 'none';
+	}
+ 
+    
+    if(password.value == ""){
+		lbpassword.innerHTML = "Favor preencher a Senha";
+		lbpassword.style.display = 'block';
+		contErro += 1;
+	}else{
+		lbpassword.style.display = 'none';
+    }
+    
+    if(contErro == 0){
+
+        firebase.auth().signInWithEmailAndPassword(email.value, password.value).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            alert(errorCode+errorMessage);
+            // ...
+          });
+    }
+
 
  })
